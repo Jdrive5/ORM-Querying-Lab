0583062195a7b64f7de583ee2a52503f2c20cf01
Monica Banda <monicabanda@Monicas-Air.lan>
2022-12-15 23:11:57 -0700

    Completed Problem 6

--- school_db/views.py
+++ school_db/views.py
@@ -289,9 +289,16 @@ VALUES ('Kyle', 'Harwood', 2022, 3.0)
 # Then query the studets table to get that student by their id
 # Print the new student's id, full name, and gpa to the terminal
 def problem_six(request):
+  
 
     # Make sure to set this equal to the primary key of the row you just created!
     student_id = 11
+    new_student = Student.objects.filter(id = 11).update(gpa = "3.5")
+    new_student = Student.objects.get(id = student_id)
+
+    print(f'Id: {new_student.id}')
+    print(f'Full Name: {new_student.first_name} {new_student.last_name}')
+    print(f'GPA: {new_student.gpa}')
 
     return complete(request)
 
